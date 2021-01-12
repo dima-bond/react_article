e6c2a998b8905ae4a2e30315d08b13bb8e926194
Dima Bond <neverpooh@gmail.com>
2021-01-12 10:22:43 +0200

    Updates

---
+++ precache-manifest.2bae68c39ec910319453b9ea2f9fd50c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07ccc8d28ce868baf749e1b3711dcbfc",
+    "url": "./index.html"
+  },
+  {
+    "revision": "61e7aeb5a2e3db6ff37f",
+    "url": "./static/js/2.d1ce54ea.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.d1ce54ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e7cd7afad009a9c74e6",
+    "url": "./static/js/main.94e872dc.chunk.js"
+  },
+  {
+    "revision": "994f72ec58672ee8392d",
+    "url": "./static/js/runtime-main.a5fa282b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.575c894ccaaaec018efa3e6d1c5de3fa.js"
+  "./precache-manifest.2bae68c39ec910319453b9ea2f9fd50c.js"
 );
 
 self.addEventListener('message', (event) => {
